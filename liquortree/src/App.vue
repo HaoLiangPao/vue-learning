1cf133c72d6a13ed1f023ad658b15971b031fd9f
Hao Liang <Hao.Liang@ibm.com>
2022-08-15 14:18:26 -0400

    Customized wrapper class of a packaged vue component

---
+++ liquortree/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LiquroTreeWrapper></LiquroTreeWrapper> -->
+    <LiquroTree></LiquroTree>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import LiquroTreeWrapper from './components/LiquroTreeWrapper.vue'
+import LiquroTree from './components/LiquroTree.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    // LiquroTreeWrapper,
+    // LiquorTree,
+    LiquroTree
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
