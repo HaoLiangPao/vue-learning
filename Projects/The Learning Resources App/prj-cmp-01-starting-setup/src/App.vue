61cc2a1bb66522af3b6b5acc5405efac24c22b87
Hao Liang <Hao.Liang@ibm.com>
2022-08-15 23:49:32 -0400

    Goal project started

---
+++ Projects/The Learning Resources App/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <StoredResourceVue :resources="storedResources"></StoredResourceVue>
+  </ul>
+</template>
+
+<script>
+import StoredResourceVue from './components/LearningResources/StoredResource.vue';
+
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentation.',
+          link: 'https://vewjs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'The must to have learning tool.',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+  components: { StoredResourceVue },
+};
+</script>
+
+<style>
+@import './styles/app.css';
+</style>
---
+++ Projects/The Learning Resources App/prj-cmp-01-starting-setup/src/components/LearningResources/StoredResource.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="resource in resources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></LearningResource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  props: ['resources'],
+  components: {
+    LearningResource,
+  },
+};
+</script>
+
+<style scoped>
+@import '../../styles/stored-resources.css';
+</style>
